1724fd8aed7a62da768b683052b7e2b0bdddb18d
diegojimenes <contato.diegojimenes@gmail.com>
2020-11-28 20:52:58 -0300

    tratando tags não fechadas

--- src/index.js
+++ src/index.js
@@ -35,12 +35,33 @@ let types = {
     }
 }
 
+let tagsQueNaoPrecisamSerFechadas = {
+    imagem: true,
+    paragrafo: true
+}
+
+let errosTags = { Aberta: false, tag: '', linha: 0 }
+
+const catchErroInTags = (firstWord, linha) => {
+    if (!firstWord.includes('*') && errosTags.Aberta == false && !tagsQueNaoPrecisamSerFechadas[firstWord]) {
+        errosTags.Aberta = true
+        errosTags.tag = firstWord
+        errosTags.linha = linha
+    } else if (firstWord.includes('*')) {
+        errosTags.Aberta = false
+        errosTags.tag = firstWord
+        errosTags.linha = linha
+    } else if (!firstWord.includes('*') && errosTags.Aberta == true && !tagsQueNaoPrecisamSerFechadas[firstWord]) {
+        throw `${errosTags.tag} na linha ${errosTags.linha} deve ser fechada`
+    }
+}
 const analiseLexica = (code) => {
     let split = code.split(';').map((b) => b.replace('\n', '')).filter((b) => b.length > 0)
-    return split.map((b) => {
-        let firstWord = b.split(' ')[0]
-        if (!types[firstWord.trim()]) firstWord = 'paragrafo'
-        return { type: types[firstWord.trim()].type, value: b, arguments: types[firstWord.trim()].arguments, parse: types[firstWord.trim()].parse }
+    return split.map((b, linha) => {
+        let firstWord = b.split(' ')[0].trim()
+        if (!types[firstWord]) firstWord = 'paragrafo'
+        catchErroInTags(firstWord, linha)
+        return { type: types[firstWord].type, value: b, arguments: types[firstWord].arguments, parse: types[firstWord].parse }
     })
 }
 
@@ -74,7 +95,7 @@ const compile = (code) => {
     return gerador(sintatica)
 }
 
-fs.readFile('./src/teste.jsHtml', 'utf8', function(err, data) {
+fs.readFile('./src/teste.jsHtml', 'utf8', function (err, data) {
     if (err) throw err;
     compile(data)
 });
