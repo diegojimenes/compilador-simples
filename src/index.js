e99b9208402fcc6722a334ef921942d83b7515ef
diegojimenes <contato.diegojimenes@gmail.com>
2020-11-28 20:23:47 -0300

    build folder

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ const analiseSintatica = (lexa) => {
     })
 }
 const gerador = (sintica) => {
-    var stream = fs.createWriteStream("my_file.html");
+    var stream = fs.createWriteStream("./build/my_file.html");
     stream.once('open', function (fd) {
         sintica.map(({ body }) => {
             stream.write(`${body.parse(body)}\n`);
