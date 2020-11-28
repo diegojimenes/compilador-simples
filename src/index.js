c3f836986755ef36e7bc4fd8936f0950e593c21e
diegojimenes <contato.diegojimenes@gmail.com>
2020-11-28 19:10:53 -0300

    basico

---
+++ src/index.js
@@ -0,0 +1,71 @@
+let code = `
+divisao;
+imagem endereco="teste";
+*divisao;
+
+divisao;
+imagem endereco="teste";
+*divisao;
+`
+
+let typesArgs = {
+    'endereco': 'src'
+}
+
+let types = {
+    'divisao': { html: '<div> ', type: 'block', parse: () => '<div>', arguments: () => null },
+    '*divisao': { html: '</div> ', type: 'close block', parse: () => '</div>', arguments: () => null },
+    'imagem': {
+        html: '<imagem> ',
+        type: 'element',
+        parse: ({arguments}) => {
+            let args = arguments.reduce((string, { type, value }) => {
+                return `${string} ${typesArgs[type]}=${value}`
+            }, '')
+            return `<img${args}/>`
+        },
+        arguments: (tag) => {
+            if (!tag) return null
+            let params = tag.split(' ').filter((b, i) => i != 0)
+            return params.map((param) => {
+                let splitParams = param.split('=')
+                return { type: splitParams[0], value: splitParams[1] }
+            })
+        }
+    }
+}
+
+const analiseLexica = (code) => {
+    let split = code.split(';').map((b) => b.replace('\n', '')).filter((b) => b.length > 0)
+    return split.map((b) => {
+        let firstWord = b.split(' ')[0]
+        return { type: types[firstWord.trim()].type, value: b, arguments: types[firstWord.trim()].arguments, parse: types[firstWord.trim()].parse }
+    })
+}
+
+const analiseSintatica = (lexa) => {
+    return lexa.map(({ type, value, arguments, parse }) => {
+        return {
+            type,
+            body: {
+                type: type,
+                name: value.split(' ')[0],
+                arguments: arguments(value),
+                parse
+            }
+        }
+    })
+}
+const gerador = (sintica) => {
+    return sintica.map(({body}) => {
+        return body.parse(body)
+    }).join('')
+}
+
+const compile = () => {
+    let lexa = analiseLexica(code)
+    let sintatica = analiseSintatica(lexa)
+    return gerador(sintatica)
+}
+
+console.log(compile())
