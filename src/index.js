88dc45fe083e9bcb472a702b0cdc65f9e583f872
diegojimenes <contato.diegojimenes@gmail.com>
2020-11-29 14:29:42 -0300

    Ajustes na captura de argumentos

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ let typesArgs = {
 
 const pegarArgumentos = (tag) => {
     if (!tag) return null
-    let params = tag.split(' ').filter((b, i) => i != 0)
+    let params = tag.replace(', ', ',').split(' ').filter((b, i) => i != 0)
     return params.map((param) => {
         let splitParams = param.split('=')
-        return { type: splitParams[0], value: splitParams[1] }
+        return { type: splitParams[0], value: [splitParams[1]] }
     })
 }
 
 const parseArgs = (arguments) => arguments.reduce((string, { type, value }) => {
-    let arg = !typesArgs[type] ? '' : `${typesArgs[type]}=${value}`
-    return `${string}${(string == '') && (arg == '') ? '': ' '}${arg}`
+    let stringValues = value[0] ? value[0] : ''
+    let valores = stringValues.replace('"', '').replace('"', '').split(',')
+    valores = valores.map((valor, i) => `${valor}${valores.length - 1 == i ? '' : '; '}`).join('')
+    let arg = !typesArgs[type] ? '' : `${typesArgs[type]}="${valores}"`
+    return `${string}${(string == '') && (arg == '') ? '' : ' '}${arg}`
 }, '')
 
 let types = {
